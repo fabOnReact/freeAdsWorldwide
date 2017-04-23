937cd3de6a6ba9bd2965a4cfc42643f1aa8c55fb
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-04-23 16:57:32 +0200

    finished testing Devise and Companytype, Companies Controller, routing and Models

--- spec/routing/routing_to_profiles.rb
+++ spec/routing/routing_to_profiles.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
-#require 'include_module_spec'
 
 RSpec.describe "routing to profiles", :type => :routing do
-#	login_user
 
-	it "routes to the homepage" do
+	it "routing not logged in users to the homepage" do
 		expect(:get => "/").to route_to(
-		:controller => "landing",
+		:controller => "landings",
 		:action => "index"
 		)
 	end
 
-	it "logged user are routed to the user page"
+	context "logged in users" do
+		
+
+		it "verifying that request does not resolve in routing error" do 			expect(:get => "/").to_not raise_error(ActionController::RoutingError)
+		end
+
+	end
 
 end
