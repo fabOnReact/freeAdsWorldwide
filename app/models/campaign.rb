d6e7ba6dac2caa6f974c0639374a490d1d09607f
Fabrizio <fabri.ber87@gmail.com>
2017-04-25 11:34:24 +0200

    need to fix campaign campaigntype FactoryGirls

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,5 @@
 class Campaign < ApplicationRecord
 	has_many :runs
+	#belongs_to :campaigntype
+	validations
 end
--- spec/factories/campaigns.rb
+++ spec/factories/campaigns.rb
@@ -2,14 +2,12 @@ FactoryGirl.define do
   factory :campaign do
     name { Faker::Company.name }
     targetcountries { Faker::Address.country }
-    association :campaigntype
-    association :company
+    campaigntype 
   end
 
   factory :invalidcampaign, :parent => :campaign do |f|
   	f.name nil
   	campaigntype nil
-  	company nil
   end
 
 end
