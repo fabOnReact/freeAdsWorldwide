8c2a82b08a6f2d46d4e3f99cab4af82aaa5f997e
Fabrizio <fabri.ber87@gmail.com>
2017-04-27 16:32:34 +0200

    fixed devise testing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
 	factory :user do |t|
 		t.email { Faker::Internet.email }
 		t.password { Faker::Internet.password(10, 20) }
+		t.country { Faker::Address.country }
 
 		factory :user_with_companies do
 			transient do
@@ -14,5 +15,16 @@ FactoryGirl.define do
 				create_list(:company, evaluator.companies_count, users: [user])
 			end
 		end
+	end
+
+	factory :invaliduser, :parent => :user do |t|
+		t.email nil
+		t.password nil
+		t.country nil
+	end
+
+	factory :completeuser, :parent => :user do |t|
+		t.firstname "Anna"
+		t.lastname "Bella"
 	end
 end
--- spec/models/run_spec.rb
+++ spec/models/run_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Run, type: :model do
+
+
 	
 end
