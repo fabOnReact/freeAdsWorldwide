41722caaccb1fd3387f9dc368c186d53e2f07811
Fabrizio <fabri.ber87@gmail.com>
2017-05-05 11:12:23 +0200

    made some fix to the application helper, still needs to be improved

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-	def title(page_title)
+	def title(page_title, variable)
 		content_for(:title) { page_title }
 	end
 	def subtitle(page_subtitle)
@@ -7,5 +7,16 @@ module ApplicationHelper
 	end
 	def paragraph(page_paragraph)
 		content_for(:paragraph) { page_paragraph }
+	end
+
+	def header(type, text)
+		case type
+			when "companies"
+				content_for(:companies) { text } 
+			when "runs"
+				content_for(:runs) { text }
+			when "campaigns"
+				content_for(:campaigns) { text }
+		end
 	end
 end
