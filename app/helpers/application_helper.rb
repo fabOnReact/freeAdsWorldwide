d2f7756919583cdd3ad80f0f49d1b175a3bd87d7
Fabrizio <fabri.ber87@gmail.com>
2017-05-05 09:33:49 +0200

    made a ugly fix to the application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,7 @@ module ApplicationHelper
 	def subtitle(page_subtitle)
 		content_for(:subtitle) { page_subtitle }
 	end
+	def paragraph(page_paragraph)
+		content_for(:paragraph) { page_paragraph }
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+    # associations
  	has_and_belongs_to_many :companies
  	has_many :campaigns, through: :companies
+ 	has_many :runs, through: :campaigns
+
+ 	# validations
  	validates_presence_of :country
+
  	def country_name
     	country = ISO3166::Country[country_code]
     	country.translations[I18n.locale.to_s] || country.name
   	end
+
 end
