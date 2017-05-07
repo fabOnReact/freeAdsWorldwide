31e3c539a4de4090acb06a8f7d668d2050bb63f4
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-05-07 15:39:08 +0200

    heroku update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -85,5 +85,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Devise
-    config.action_mailer.default_url_options = { host: 'https://freeadsworldwide.herokuapp.com/'}
+    config.action_mailer.default_url_options = { host: 'https://growstarups.xyz'}
 end
