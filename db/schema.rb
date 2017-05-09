eda79ae9b6e00539143514f3fbe32a4f91319a49
Fabrizio <fabri.ber87@gmail.com>
2017-05-09 10:50:40 +0200

    simple subscription box and style

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508162520) do
+ActiveRecord::Schema.define(version: 20170509081303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,12 @@ ActiveRecord::Schema.define(version: 20170508162520) do
     t.integer  "ownads"
     t.string   "status"
     t.index ["campaign_id"], name: "index_runs_on_campaign_id", using: :btree
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
