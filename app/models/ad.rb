715faa55d92eb02ce070ee8ebd776da15e96a85b
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-05-13 14:00:49 +0200

    trying to fix ads visits

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,9 +1,26 @@
 class Ad < ApplicationRecord
-  belongs_to :company
-  belongs_to :run
+	belongs_to :company
+	belongs_to :run
 
-  def self.visits(ad)
-	ad.visits += 1
-    ad.save
-  end
+	def self.visits(ad)
+		ad.visits += 1
+		ad.save
+	end
+
+
+	def self.website(ad)
+		website = ad.company.website
+		
+		if website.start_with?("http://")
+			return website
+		elsif website.start_with?("https://")
+			return website
+		elsif website.start_with?("https://www.")
+			return website
+		elsif website.start_with?("www.")
+			return website
+		else 
+			return website = "www." + website
+		end
+	end  
 end
