7c34c5b5a8d255066feaf3dbed95ba51bfd2e3a4
Fabrizio <fabri.ber87@gmail.com>
2017-05-16 10:43:18 +0200

    fixed the olark chat

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require turbolinks
 //= require bootstrap-sprockets
 //= require awesome-landing-page
+//= require olark
+//= require turbolinks
