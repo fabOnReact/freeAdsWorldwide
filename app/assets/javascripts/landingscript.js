f2aa3d4828f4feb9ce3d2f3e7724d1917ac28b9b
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-05-18 09:33:19 +0200

    new effects for buttons

--- app/assets/javascripts/landingscript.js
+++ app/assets/javascripts/landingscript.js
@@ -1,6 +1,12 @@
 var ready = function() {
 
-    $('#parallax-button').mouseover(function(){
+    $('#bouncing-button').mouseover(function(){
+        var $this = $(this);
+        if (!$this.is(":animated"))
+            $(this).effect("bounce", { times: 5 }, 2000);
+    });
+
+    $('.buttons .btn').mouseover(function(){
         var $this = $(this);
         if (!$this.is(":animated"))
             $(this).effect("bounce", { times: 5 }, 2000);
