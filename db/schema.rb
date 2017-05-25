6ef5ab2f3aba9a0af39298baa2a8971d2114128b
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-05-25 18:16:29 +0200

    cleared counters - ok to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525084804) do
+ActiveRecord::Schema.define(version: 20170525161326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 20170525084804) do
     t.integer  "run_id"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
-    t.integer  "visits",        default: 0
     t.boolean  "selfpromotion"
+    t.integer  "visits",        default: 0
     t.index ["company_id"], name: "index_ads_on_company_id", using: :btree
     t.index ["run_id"], name: "index_ads_on_run_id", using: :btree
     t.index ["visits"], name: "index_ads_on_visits", using: :btree
